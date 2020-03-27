71ec5116d091a19aabbb21a7ad38346e608a2276
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2020-03-27 12:59:21 +0100

    Initial commit

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+# AzureRm Run Command Updated for AzureRM v2.0 
+
+# Local Variables
 locals {
   settings_windows = {
     script   = "${compact(concat(list(var.command), split("\n", var.script)))}"
@@ -11,21 +14,30 @@ locals {
   }
 }
 
+# Data Resource Group
 data "azurerm_resource_group" "main" {
   name = "${var.resource_group_name}"
 }
 
-data "azurerm_virtual_machine" "main" {
-  name                = "${var.virtual_machine_name}"
+# Linux Virtual Machine
+data "azurerm_linux_virtual_machine" "linux_main" {
+  id                  = "${var.virtual_machine_id}"
   resource_group_name = "${data.azurerm_resource_group.main.name}"
 }
 
+# Windows Virtual Machine
+data "azurerm_windows_virtual_machine" "windows_main" {
+  id                  = "${var.virtual_machine_id}"
+  resource_group_name = "${data.azurerm_resource_group.main.name}"
+}
+
+# Azure Virtual Machine Extension for Linux
 resource "azurerm_virtual_machine_extension" "linux" {
   count                      = "${lower(var.os_type) == "linux" ? 1 : 0}"
   name                       = "${var.virtual_machine_name}-run-command"
   location                   = "${data.azurerm_resource_group.main.location}"
   resource_group_name        = "${data.azurerm_resource_group.main.name}"
-  virtual_machine_name       = "${data.azurerm_virtual_machine.main.name}"
+  virtual_machine_id         = "${data.azurerm_linux_virtual_machine.linux_main.id}"
   publisher                  = "Microsoft.CPlat.Core"
   type                       = "RunCommandLinux"
   type_handler_version       = "1.0"
@@ -34,12 +46,13 @@ resource "azurerm_virtual_machine_extension" "linux" {
   tags                       = "${var.tags}"
 }
 
+# Azure Virtual Machine Extension for Windows
 resource "azurerm_virtual_machine_extension" "windows" {
   count                      = "${lower(var.os_type) == "windows" ? 1 : 0}"
   name                       = "${var.virtual_machine_name}-run-command"
   location                   = "${data.azurerm_resource_group.main.location}"
   resource_group_name        = "${data.azurerm_resource_group.main.name}"
-  virtual_machine_name       = "${data.azurerm_virtual_machine.main.name}"
+  virtual_machine_id         = "${data.azurerm_virtual_machine.windows_main.id}"
   publisher                  = "Microsoft.CPlat.Core"
   type                       = "RunCommandWindows"
   type_handler_version       = "1.1"
